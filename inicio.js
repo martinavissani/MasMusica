716c28266fb46d12e8b56d8ce725c3048186266e
Martina <91103968+martinavissani@users.noreply.github.com>
2022-10-19 21:24:47 -0300

    3° entrega

---
+++ inicio.js
@@ -0,0 +1,154 @@
+window.onload = function()
+{ 
+  let linktfj = document.getElementById("linktfj")
+  let linkeh = document.getElementById("linkeh")
+  let linkfroot = document.getElementById("linkfroot")
+  let linklf = document.getElementById("linklf")
+  let linkad = document.getElementById("linkad")
+  let linkdna = document.getElementById("linkdna")
+  let linksalute = document.getElementById("linksalute")
+  let linkgw = document.getElementById("linkgw")
+  let linkgd = document.getElementById("linkgd")
+  let linklm5 = document.getElementById("linklm5")
+  let linkconfetti = document.getElementById("linkconfetti")
+  let linkbu = document.getElementById("linkbu")
+  let linkdl = document.getElementById("linkdl")
+  let linkfn = document.getElementById("linkfn")
+  let linkyt = document.getElementById("linkyt")
+  let linkme = document.getElementById("linkme")
+  let linkdw = document.getElementById("linkdw")
+  let linksweetener = document.getElementById("linksweetener")
+  let linktun = document.getElementById("linktun")
+  let linkpositions = document.getElementById("linkpositions")
+  let linklungs = document.getElementById("linklungs")
+  let linkceremonials = document.getElementById("linkceremonials")
+  let linkhbhbhb = document.getElementById("linkhbhbhb")
+  let linkhah = document.getElementById("linkhah")
+  let linkdf = document.getElementById("linkdf")
+  let link19 = document.getElementById("link19")
+  let link21 = document.getElementById("link21")
+  let link25 = document.getElementById("link25")
+  let link30 = document.getElementById("link30")
+  let linkbtd = document.getElementById("linkbtd")
+
+  linktfj.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/3dWObfa3vKyudzgmh53Xzg?si=KdynGznBQvORImDqJxgN7w")
+  })
+
+  linkeh.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/3LsZ6ggAetZSzxl4hWOB3S?si=7TK_p7HrQ-2ujken6CfmQw")
+  })
+
+  linkfroot.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/1u2ACTYzVNK3vSLG0Ah4H3?si=K-w7h6v8RlKlK5rvxchi1Q")
+  })
+
+  linklf.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/0CUxS3KfHNuDpUUjbAewV3?si=W0YvQ9LsRoCZhtqdhkXxZA")
+  })
+
+  linkad.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/5fa6oWFXuvaVkY49mfOsRd?si=DET64A2mTWO-Wj7XHQNsAg")
+  })
+
+  linkdna.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/4eqzUBHUk78dldidne4k4m")
+  })
+
+  linksalute.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/0mAToSIUJxY7bae4MnIQOV")
+  })
+
+  linkgw.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/2FnfZiFMv7bmXAIATOJDHs")
+  })
+
+  linkgd.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/4w49hcqZt66HZX0MJHfW0m")
+  })
+
+  linklm5.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/7Ho8gAx4haSxv1eFLQwvTj")
+  })
+
+  linkconfetti.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/33a8Ha3pE7fo2o4T4xLabF?si=vN7hJUIkR92mAHReyUXKXQ")
+  })
+
+  linkbu.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/6XNrM2YkzSXyQ9hekFOgLN")
+  })
+
+  linkdl.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/01sfgrNbnnPUEyz6GZYlt9?si=m7FpVj3AQXuo87HkS4c35g")
+  })
+
+  linkfn.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/7fJJK56U9fHixgO0HQkhtI?si=wSS3-ntnQ-SYP6twmNs-JQ")
+  })
+
+  linkyt.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/6czdbbMtGbAkZ6ud2OMTcg?si=bJY5nSZKRUCHC1UlrnXg9w")
+  })
+
+  linkme.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/6EVYTRG1drKdO8OnIQBeEj?si=s5apr00JQXWvtztBZTVMiw")
+  })
+
+  linkdw.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/3pdKKSqqLVIKmRTGw0x2N7?si=ZtUeeKNZRNGGRQnIoYAhMA")
+  })
+
+  linksweetener.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/3tx8gQqWbGwqIGZHqDNrGe?si=TSmlsI_PQkGYb5fKjWcG_Q")
+  })
+
+  linktun.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/2fYhqwDWXjbpjaIJPEfKFw?si=XQ3jq6UCROukUb4779WGrw")
+  })
+
+  linkpositions.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/3euz4vS7ezKGnNSwgyvKcd?si=yO6nn81vQj2EYcy4sLKv3Q")
+  })
+
+  linklungs.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/6hs55qjerNNOi4ZT5bOhxV?si=sCYVPWPCSBSxprrvJm84AA")
+  })
+
+  linkceremonials.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/5DMgU1P55Su3EVXGvgID1p")
+  })
+
+  linkhbhbhb.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/2btszoya78vyT8fwelmVnz")
+  })
+
+  linkhah.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/0pKZJj9GzcKPCS8r4IaksA?si=4R1ghRw1RtyqlQCH23sb4Q")
+  })
+
+  linkdf.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/4ohh1zQ4yybSK9FS7LLyDE?si=ZNbtwZVgQ9aIuwrbJsrORg")
+  })
+
+  link19.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/59ULskOkBMij4zL8pS7mi0?si=bPgdHnhwQg6czYQSRwXjjw")
+  })
+
+  link21.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/0Lg1uZvI312TPqxNWShFXL?si=vPU7hvJoT1aq9g8Em8Q8oA")
+  })
+
+  link25.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/3AvPX1B1HiFROvYjLb5Qwi?si=TFr3ZD6xRpC2T1njR__9xQ")
+  })
+
+  link30.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/21jF5jlMtzo94wbxmJ18aa?si=FDdfw8-IT3er8vt6Pspxag")
+  })
+  
+  linkbtd.addEventListener("click", function () {
+    window.open("https://open.spotify.com/album/4vXt6IpMcSnqonljffWlMI?si=GgyPBdx4SDS0WHY6MkjnHQ")
+  })
+
+}
